5658dcd95fc11817cc4e2029e80212e2680bf19c
Ron Lievens <ron.lievens@gvb.nl>
2025-02-18 17:55:47 +0100

    sync

--- changelog/v1/schema/clients.sql
+++ changelog/v1/schema/clients.sql
@@ -1,13 +1,13 @@
 -- liquibase formatted sql
 -- changeset ronlievens:schema-1
-CREATE TABLE activities
+CREATE TABLE clients
 (
     id           BIGINT                 NOT NULL AUTO_INCREMENT,
     name         VARCHAR(50)            NOT NULL,
     reporting    BOOLEAN  DEFAULT FALSE NOT NULL,
     registered   DATETIME DEFAULT NOW() NOT NULL,
     unregistered DATETIME,
-    CONSTRAINT activities_pk PRIMARY KEY (id)
+    CONSTRAINT clients_pk PRIMARY KEY (id)
 ) ENGINE = InnoDB
   CHARACTER SET 'utf8'
   COLLATE 'utf8_unicode_ci';
--- changelog/v1/schema/tickets.sql
+++ changelog/v1/schema/tickets.sql
@@ -2,15 +2,20 @@
 -- changeset ronlievens:schema-1
 CREATE TABLE tickets
 (
-    id           BIGINT                   NOT NULL AUTO_INCREMENT,
+    id          BIGINT                 NOT NULL AUTO_INCREMENT,
+    client_id   BIGINT,
     ticket_type  ENUM ('REPAIR', 'ISSUE') NOT NULL,
     equipment_id BIGINT,
-    description  JSON,
-    registered   DATETIME DEFAULT NOW()   NOT NULL,
+
+    subject     varchar(255)           NOT NULL,
+    description TEXT,
+    details     JSON,
+    registered  DATETIME DEFAULT NOW() NOT NULL,
     unregistered DATETIME,
 
     CONSTRAINT tickets_pk PRIMARY KEY (id),
-    CONSTRAINT tickets_fk_1 FOREIGN KEY (equipment_id) REFERENCES equipment (id) ON UPDATE RESTRICT ON DELETE CASCADE
+    CONSTRAINT tickets_fk_1 FOREIGN KEY (equipment_id) REFERENCES equipment (id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    CONSTRAINT tickets_fk_2 FOREIGN KEY (client_id) REFERENCES clients (id) ON UPDATE RESTRICT ON DELETE CASCADE
 ) ENGINE = InnoDB
   CHARACTER SET 'utf8'
   COLLATE 'utf8_unicode_ci';
