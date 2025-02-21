95475028b1d22a51f12cc3ac8390bc024bc812d7
Ron Lievens <ron.lievens@gvb.nl>
2025-02-21 14:32:24 +0100

    sync

--- changelog/v1/schema/equipment.sql
+++ changelog/v1/schema/equipment.sql
@@ -9,11 +9,13 @@ CREATE TABLE equipment
     model         VARCHAR(255),
     specification JSON                   NOT NULL,
     customer_id   BIGINT,
+    client_id BIGINT,
     registered    DATETIME DEFAULT NOW() NOT NULL,
     unregistered  DATETIME,
 
     CONSTRAINT equipment_pk PRIMARY KEY (id),
-    CONSTRAINT equipment_fk_1 FOREIGN KEY (customer_id) REFERENCES individuals (id) ON UPDATE RESTRICT ON DELETE CASCADE
+    CONSTRAINT equipment_fk_1 FOREIGN KEY (customer_id) REFERENCES individuals (id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    CONSTRAINT equipment_fk_2 FOREIGN KEY (client_id) REFERENCES clients (id) ON UPDATE RESTRICT ON DELETE CASCADE
 ) ENGINE = InnoDB
   CHARACTER SET 'utf8'
   COLLATE 'utf8_unicode_ci';
